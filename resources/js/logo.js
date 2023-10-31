36d1e5eeda62e93ab36c3a97c912fb9651cc80e8
GGMachina <ggonza55@uic.edu>
2023-10-31 01:56:52 -0500

    Updated icon and favicon

--- resources/js/logo.js
+++ resources/js/logo.js
@@ -10,14 +10,14 @@ ctx.translate(canvas.width / 2, canvas.height / 2);
 // To make it easier, make them in polar coordinates then convert to cartesian
 // Top & bottom are pointy ends
 // diameter of hex
-var diameter = canvas.width / 2.2;
+var radius = canvas.width / 2.2;
 var polarPoints = [
-    [90, diameter],
-    [30, diameter],
-    [330, diameter],
-    [270, diameter],
-    [210, diameter],
-    [150, diameter]
+    [90, radius],
+    [30, radius],
+    [330, radius],
+    [270, radius],
+    [210, radius],
+    [150, radius]
 ];
 
 // Function to convert polar coordinates to cartesian
@@ -57,23 +57,21 @@ ctx.fill();
 ctx.stroke();
 // fill the hexagon
 
-//TODO: Convert this to be drawn on the canvas instead of SVG
+// DONE: Convert this to be drawn on the canvas instead of SVG
 // TODO: once this is done, save the finished canvas image and set it as the sites favicon
 
 
 // declare where the points are for one of the G's that we will draw on the logo. In polar coordinates
-var innerDiameter = diameter * 0.8;
+var innerRadius = radius * 0.8;
 var gPoints = [
-    [-103, innerDiameter / 2.1], // 0
-    [-95, innerDiameter - 10], // 1
-    [-150, innerDiameter], // 2
-    [-210, innerDiameter], // 3
-    [-265, innerDiameter - 10], // 4
-    [-260, innerDiameter / 8], // 5
-    [-210, innerDiameter * 3 / 5], // 6
+    [-103, innerRadius / 2.1], // 0
+    [-95, innerRadius - 8.5], // 1
+    [-150, innerRadius], // 2
+    [-210, innerRadius], // 3
+    [-265, innerRadius - 8.5], // 4
+    [-260, innerRadius / 8], // 5
+    [-210, innerRadius * 3 / 5], // 6
 ];
-
-
 
 // Map the polar coordinates into cartesian coordinates and put in a new array
 var gCartesianPoints = [];
@@ -119,3 +117,15 @@ for (var i = 1; i < gCartesianPoints.length; i++) {
     ctx.lineTo(gCartesianPoints[i].x, gCartesianPoints[i].y);
 }
 ctx.stroke();
+
+// // save the canvas as an image
+// var canvasImage = canvas.toDataURL("../images/");
+// // Apply it to an image
+// var img = new Image();
+// img.src = canvasImage;
+
+// var link = document.createElement('a');
+// link.download = 'favicon.png';
+// link.href = canvasImage;
+
+// link.click();
